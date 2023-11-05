6cd3f7a86e912f7b83971161e7d373b6bacdb1cc
aydanyunus <aydanyunus98@gmail.com>
2023-11-05 23:30:16 +0400

    header ui update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { BiSolidCameraMovie } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import "./header.scss";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <div class="header__logo">
-          <Link>
-            <BiSolidCameraMovie />
-          </Link>
-        </div>
-        <div class="header__search">
-          <input type="search" placeholder="Search any movies or TV shows" />
+        <div className="header__wrapper">
+          <div className="header__logo">
+            <Link to="/" className="header__logo--link">
+              <BiSolidCameraMovie />
+            </Link>
+          </div>
+          <div className="header__search">
+            <input className="header__search--input" type="search" placeholder="Search any movies or TV shows" />
+          </div>
+          <div className="header__nav">
+            <nav className="nav">
+              <Link to="/favorites">Movies</Link>
+              <Link to="/favorites">Favorites</Link>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
