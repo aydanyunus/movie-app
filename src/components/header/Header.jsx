ba724ea75c701379d5041e6c1b4a78f90cfb0a96
aydanyunus <aydanyunus98@gmail.com>
2023-11-07 23:26:12 +0400

    mobile ui added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiSolidCameraMovie } from "react-icons/bi";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 import "./header.scss";
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(true);
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
-          <div className="header__logo">
-            <Link to="/" className="header__logo--link">
-              <BiSolidCameraMovie />
-            </Link>
+          <div className="header__top">
+            <div className="header__logo">
+              <Link to="/" className="header__logo--link">
+                <BiSolidCameraMovie />
+              </Link>
+            </div>
+            <div className="header__search">
+              <input
+                className="header__search--input"
+                type="search"
+                placeholder="Search any movies or TV shows"
+              />
+               <input
+                className="header__search--mobile-input"
+                type="search"
+                placeholder="Search"
+              />
+            </div>
           </div>
-          <div className="header__search">
-            <input className="header__search--input" type="search" placeholder="Search any movies or TV shows" />
-          </div>
+
           <div className="header__nav">
             <nav className="nav">
-              <Link to="/favorites">Movies</Link>
-              <Link to="/favorites">Favorites</Link>
+              <NavLink to="/movies">Movies</NavLink>
+              <NavLink to="/shows">TV Shows</NavLink>
+              <NavLink to="/favorites">Favorites</NavLink>
             </nav>
+            <div className="mobile__menu">
+              <button
+                type="button"
+                className="menu__btn"
+                onClick={() => setMobileMenu(!mobileMenu)}
+              >
+                {mobileMenu ? <HiOutlineMenuAlt3 /> : <AiOutlineClose />}
+              </button>
+            </div>
           </div>
         </div>
       </div>
+      {!mobileMenu ? (
+        <div className="menu__wrapper">
+          <div className="container">
+            <ul className="menu__items">
+              <li className="item">
+                <NavLink to="/movies">Movies</NavLink>
+              </li>
+              <li className="item">
+                <NavLink to="/shows">TV Shows</NavLink>
+              </li>
+              <li className="item">
+                <NavLink to="/favorites">Favorites</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </header>
   );
 };
