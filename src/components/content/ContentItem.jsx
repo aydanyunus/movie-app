5a27395ec7884f07073a7acaa3d83533eb00eefc
aydanyunus <aydanyunus98@gmail.com>
2023-12-21 23:50:00 +0400

    update style

--- src/components/content/ContentItem.jsx
+++ src/components/content/ContentItem.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import { FaRegHeart } from "react-icons/fa";
 
 const ContentItem = ({ movie }) => {
   return (
     <div className="item">
-      <img src={`${movie.Poster}`} alt="img" />
-      <div className="details">
-        <h5 className="item__title">{movie.Title}</h5>
-        <div className="rating">
-          <AiFillStar />
-          <span className="imdb">7.0</span>
+      <img src={`${movie.Poster}`} alt="img" className="item-img" />
+      <div className="flex">
+        <div className="details">
+          <h5 className="item__title">{movie.Title}</h5>
+          <div className="rating">
+            <AiFillStar />
+            <span className="imdb">7.0</span>
+          </div>
+          <h6 className="item__year">
+            <span>{movie.Year}</span>
+          </h6>
         </div>
-        <h6 className="item__year">
-          <span>{movie.Year}</span>
-        </h6>
+        <button className="like-btn" type="button">
+          <FaRegHeart />
+        </button>
       </div>
     </div>
   );
